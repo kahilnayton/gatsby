842446dd5b83d4ac841bd65cda22c7bc77545789
kahil nayton <kahilnayton@gmail.com>
2020-01-05 21:01:01 -0500

    init

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus assumenda similique voluptatibus odit dolores quae unde expedita reiciendis commodi voluptate cumque reprehenderit, laudantium sit cum quod porro est perspiciatis. Temporibus ut tempore neque et iusto corporis voluptas blanditiis dignissimos adipisci.</p>
+      
+    </div>
+)
+
+export default AboutPage;
---
+++ src/pages/services.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const ServicesPage = () => (
+  <div>
+    <h1>Services</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+      assumenda similique voluptatibus odit dolores quae unde expedita
+      reiciendis commodi voluptate cumque reprehenderit, laudantium sit cum quod
+      porro est perspiciatis. Temporibus ut tempore neque et iusto corporis
+      voluptas blanditiis dignissimos adipisci.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+      assumenda similique voluptatibus odit dolores quae unde expedita
+      reiciendis commodi voluptate cumque reprehenderit, laudantium sit cum quod
+      porro est perspiciatis. Temporibus ut tempore neque et iusto corporis
+      voluptas blanditiis dignissimos adipisci.
+    </p>
+  </div>
+)
+
+export default ServicesPage
